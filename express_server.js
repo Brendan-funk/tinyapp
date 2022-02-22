056016d15a21357d8dc4652ad6c8fd51f8fe47d4
Brendan-funk <brendanzfunk@gmail.com>
2022-02-22 01:40:09 +0000

    added new url template and generate random string function

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -16,10 +18,21 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+
+
 
 app.get("/urls/:shortURL", (req,res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],};
@@ -33,5 +46,13 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
- 
+const generateRandomString = function() {
+   const charList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   const output = '';
+   const shortUrlLength = 6;
+   for(let i = 0; i < shortUrlLength; i++) {
+     const randomNumber = Math.floor(Math.random() * charList.length);
+     output += charList.charAt(randomNumber);
+   }
+   return output;
+ };
