09741d05eda59235d00cc756fd4ba05ddc844f70
Brendan-funk <brendanzfunk@gmail.com>
2022-02-24 01:09:02 +0000

    added registration ejs

--- express_server.js
+++ express_server.js
@@ -17,6 +17,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_register", templateVars);
+})
 app.get("/urls/new", (req, res) => {
   templateVars = {username: req.cookies.username}
   res.render("urls_new",templateVars);
